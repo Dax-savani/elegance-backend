ea97f6e8cd5e159789ba7746fbfae66b706325bc
Dax-savani <dakshsavani15@gmail.com>
2024-09-30 14:07:01 +0530

    bug fixes

--- index.js
+++ index.js
@@ -21,7 +21,10 @@ connectionDB(process.env.DB_CONNECTION_STRING);
 
 //Middlewares
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3000",  
+    credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended: false}));
