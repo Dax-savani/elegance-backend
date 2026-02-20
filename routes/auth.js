4906ac994bfaf8db299580308ee6fce8a9ed937c
Savani <163087650+Dax-savani@users.noreply.github.com>
2026-02-21 00:47:01 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { register ,login,me , userEdit , passwordEdit} = require('../controllers/user');
 const router = express.Router();
 const {auth} = require("../middlewares/auth");
-//         
+       
 router.post('/register',register);
 
 router.put('/',auth,userEdit);
